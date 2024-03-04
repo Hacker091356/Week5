946fae9711bf7ef2a1dd6525c527ccdccedeab8f
Hacker091356 <ninno2812@gmail.com>
2024-03-04 14:57:42 -0500

    hh

---
+++ Election.java
@@ -0,0 +1,14 @@
+/*******
+ * Name: Neil Oza
+ * Date 3/4/2024
+ * Program: Display results of an election
+ */
+import java.text.NumberFormat;
+public class Election {
+    public static void main(String[] args) {
+        double Awbrey = 49.71;
+        NumberFormat.getPercentInstance();
+
+        System.out.println("Candidate       Votes   Percentages\n Awbrey        616618  " +  percent.format(awbrey));
+    }
+} 
