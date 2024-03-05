ed300d3819d1a87b9a67ac426d4d980de13a2902
Hacker091356 <ninno2812@gmail.com>
2024-03-05 14:59:40 -0500

    Updated from school

--- Election.java
+++ Election.java
@@ -3,12 +3,22 @@
  * Date 3/4/2024
  * Program: Display results of an election
  */
+// Importing 
 import java.text.NumberFormat;
+import java.util.Scanner;
 public class Election {
     public static void main(String[] args) {
-        double Awbrey = 49.71;
-        NumberFormat.getPercentInstance();
-
-        System.out.println("Candidate       Votes   Percentages\n Awbrey        616618  " +  percent.format(awbrey));
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the number of votes Awbrey recieved in NY, NJ, and CT:");
+        double votesNYA = userInput.nextDouble();
+        double votesNJA = userInput.nextDouble();
+        double votesCTA = userInput.nextDouble();
+        System.out.println("Enter the number of votes Martinez recieved in NY, NJ, and CT:");
+        double votesNYM = userInput.nextDouble();
+        double votesNJM = userInput.nextDouble();
+        double votesCTM = userInput.nextDouble();
+        
+        userInput.close();
+       
     }
 } 
