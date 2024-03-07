97b455bdc05010963c4251de304aa1e723981471
Hacker091356 <ninno2812@gmail.com>
2024-03-07 14:26:43 -0500

    hh

--- timeConversion.java
+++ timeConversion.java
@@ -5,10 +5,12 @@ public class timeConversion {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter the time in minutes:");
         int timeInMinutes = userInput.nextInt();
-        int onesMinute = timeInMinutes % 60;
-        int tensMinute = onesMinute/10;
+        int onesMinute = (timeInMinutes % 60)%10;
+        int tensMinute = (timeInMinutes % 60)/10;
         int timeInHours = (timeInMinutes-onesMinute)/60;
         userInput.close();
+        System.out.print(onesMinute);
+        System.out.print(tensMinute);
         System.out.println("The time in hours is " + timeInHours +":" + tensMinute + onesMinute);
     }
 }
